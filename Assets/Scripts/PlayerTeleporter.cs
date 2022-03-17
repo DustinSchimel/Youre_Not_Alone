c8b29559e95343569b322c141ceb4be311ce76ef
Dustin Schimel <dustin.schimel@gmail.com>
2022-03-16 18:14:05 -0600

    Revert "Added unlocking double jump"
    This reverts commit d5b795336b4571571535b40066885172b20265d9.

--- Assets/Scripts/PlayerTeleporter.cs
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -7,25 +7,14 @@ public class PlayerTeleporter : MonoBehaviour
 {
     private GameObject currentTeleporter;
 
-    CharacterController2D controller;
+    // Update is called once per frame
 
-    private void Awake()
-    {
-        controller = this.gameObject.GetComponent<CharacterController2D>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision) 
     {
-        GameObject tp = collision.gameObject;
-
         if (collision.CompareTag("Teleporter"))
         {
-            if (tp.name == "Teleporter1")
-            {
-                controller.doubleJumpEnabled = true;
-            }
-
-            transform.position = collision.gameObject.GetComponent<Teleporter>().GetDestination().position;      
+                transform.position = collision.gameObject.GetComponent<Teleporter>().GetDestination().position;      
         }
     }
 }
